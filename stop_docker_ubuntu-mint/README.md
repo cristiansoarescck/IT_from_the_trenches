2ede5d537c12ca34eab6a7dccc7dc0b156a56f0c
Cristian Soares <cristian.soares.cck@gmail.com>
2025-11-22 17:17:41 -0300

    add script

---
+++ stop_docker_ubuntu-mint/README.md
@@ -0,0 +1,3 @@
+# Stop and Disable Docker
+A .sh for stop and disable docker services to save resource 
+It will be need manual start docker 
---
+++ stop_docker_ubuntu-mint/stop_docker.sh
@@ -0,0 +1,9 @@
+
+# Stop Docker services
+sudo systemctl stop docker.service docker.socket
+
+# Disable Docker
+sudo systemctl disable docker.service docker.socket
+
+# For enable and start docker, run this command:
+sudo systemctl start docker
